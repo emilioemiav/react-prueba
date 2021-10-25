1fe20c2aac5bb3d86fd49c174abfa7679f012461
emilioaguirrevicentin <71348992+emilioaguirrevicentin@users.noreply.github.com>
2021-10-25 14:48:05 -0300

    cree un componente de prueba

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Componente from './Componente';
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      
+        {/* <Componente/> //para abrir y cerrar en la misma linea /al final */}
+       <Componente/>
+       
+
+    
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Componente.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+function Componente (){ //
+return (<div>hola mundo</div>);
+}
+export default Componente
